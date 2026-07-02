e7ea516ad6bfa0be8f07a808a6e1c3e91729ae1a
egikhairuna <egikhairuna@gmail.comgit config --global user.email egikhairuna@gmail.com>
2026-07-02 13:23:59 +0700

    fix(csp): add Google Analytics and Google domains to connect-src

--- next.config.ts
+++ next.config.ts
@@ -52,7 +52,7 @@ const nextConfig: NextConfig = {
               "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://unpkg.com",
               "img-src 'self' blob: data: https://vps.jamesboogie.com https://jamesboogie.com https://*.wp.com https://images.unsplash.com https://secure.gravatar.com https://www.google.com https://maps.gstatic.com https://www.googletagmanager.com https://www.facebook.com https://connect.facebook.net https://basemaps.cartocdn.com https://*.basemaps.cartocdn.com https://*.tile.openstreetmap.org https://unpkg.com",
               "font-src 'self' data: https://fonts.gstatic.com",
-              "connect-src 'self' https://vps.jamesboogie.com https://apiv2.jne.co.id:10205 https://www.googletagmanager.com https://connect.facebook.net https://www.facebook.com https://basemaps.cartocdn.com https://*.basemaps.cartocdn.com",
+              "connect-src 'self' https://vps.jamesboogie.com https://apiv2.jne.co.id:10205 https://www.googletagmanager.com https://connect.facebook.net https://www.facebook.com https://basemaps.cartocdn.com https://*.basemaps.cartocdn.com https://www.google-analytics.com https://www.google.com",
               "frame-src 'self' https://www.google.com https://www.googletagmanager.com https://www.facebook.com",
               "media-src 'self' https://vps.jamesboogie.com",
               "frame-ancestors 'none'",
